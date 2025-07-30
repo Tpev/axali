eefcecaed0fdf2450c8429c88feabb64000b163f
Epicpp <peverelli.t@gmail.com>
2025-07-30 11:13:26 +0200

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+		<script id="vtag-ai-js" async src="https://r2.leadsy.ai/tag.js" data-pid="1g93g8wFEfyfOg7bE" data-version="062024"></script>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
